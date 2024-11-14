2560ac4cc9257e4c18b0b73d53f12064dd4fcca9
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-11-14 12:52:13 +0330

    feat: config typeorm

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from "@nestjs/common";
 import { DoctorsModule } from "../doctors/doctors.module";
 import { ConfigModule } from "@nestjs/config";
 import envConfig from "../../configs/env.config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeormConfig } from "../../configs/typeorm.config";
 
 @Module({
   imports: [
     DoctorsModule,
     ConfigModule.forRoot(envConfig()),
+    TypeOrmModule.forRootAsync(typeormConfig())
   ],
 })
 export class AppModule {}
