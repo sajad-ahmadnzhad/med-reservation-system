b49abd39bd499997f6ef8d46ae3e7efd71629ec0
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-11-14 13:48:11 +0330

    feat: config swagger

---
+++ src/configs/swagger.config.ts
@@ -0,0 +1,27 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SecuritySchemeObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
+
+export const swaggerConfigInit = (app: INestApplication) => {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("med reservation system")
+    .setDescription("Patient appointment booking system")
+    .setVersion("0.0.1")
+    .addBearerAuth(swaggerAuthConfig(), "Authorization")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup("swagger", app, document, {
+    jsonDocumentUrl: "swagger/json",
+  });
+};
+
+function swaggerAuthConfig(): SecuritySchemeObject {
+  return {
+    scheme: "bearer",
+    type: "http",
+    in: "header",
+    bearerFormat: "JWT",
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app/app.module";
 import { Logger } from "@nestjs/common";
+import * as express from "express";
+import { swaggerConfigInit } from "./configs/swagger.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const { PORT = 3000 } = process.env;
-  const logger = new Logger()
+  const logger = new Logger();
+
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ extended: true }));
+
+  app.setGlobalPrefix("api");
+
+  //* Config and Init Swagger
+  swaggerConfigInit(app);
 
   await app.listen(PORT, () => {
-    logger.log(`Server running on port ${PORT}`)
+    logger.log(`Server running on port ${PORT}`);
   });
 }
 bootstrap();
