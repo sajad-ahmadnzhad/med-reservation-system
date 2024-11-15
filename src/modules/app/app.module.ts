c07e2969c5e4ca4af48cea114ed86db0a5214c67
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-11-16 02:10:52 +0330

    feat: remove helmet options

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -41,17 +41,7 @@ import { AuthModule } from "../auth/auth.module";
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(helmet({
-  crossOriginEmbedderPolicy: false,
-  contentSecurityPolicy: {
-    directives: {
-      imgSrc: [`'self'`, 'data:', 'apollo-server-landing-page.cdn.apollographql.com'],
-      scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
-      manifestSrc: [`'self'`, 'apollo-server-landing-page.cdn.apollographql.com'],
-      frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
-    },
-  },
-}), cookieParser())
+      .apply(helmet(), cookieParser())
       .forRoutes({ path: "*", method: RequestMethod.ALL });
   }
 }
