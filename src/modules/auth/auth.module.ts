ca500da51f275cb768902c2580e898bc264c0780
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-11-16 19:26:10 +0330

    feat: set typeorm cloumns in user entity

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthResolver } from "./auth.resolver";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthResolver, AuthService],
 })
 export class AuthModule {}
--- src/modules/auth/entities/user.entity.ts
+++ src/modules/auth/entities/user.entity.ts
@@ -1,7 +1,30 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from "@nestjs/graphql";
+import { BaseEntity } from "../../../common/abstracts/base.entity";
+import { Column, Entity } from "typeorm";
 
 @ObjectType()
-export class User {
-  @Field(() => Int)
-  exampleField: number;
+@Entity()
+export class User extends BaseEntity {
+  @Field(() => String)
+  @Column({ nullable: false, type: "varchar" })
+  full_name: string;
+
+  @Field(() => String)
+  @Column({ nullable: false, type: "varchar" })
+  email: string;
+
+  @Field(() => String)
+  @Column({ nullable: false, type: "varchar" })
+  phone_number: string;
+
+  @Field(() => String)
+  @Column({ nullable: false, type: "varchar" })
+  role: string;
+
+  @Field(() => String)
+  @Column({ nullable: false, type: "varchar" })
+  avatar_url: string;
+
+  @Column({ nullable: true, type: "varchar", select: false })
+  password?: string;
 }
